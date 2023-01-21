a038c19f6c058dd33fa5ce478db4e95a5625e547
Maciej Urbas <maciej.urbas3@gmail.com>
2023-01-21 01:06:55 +0100

    :sparkles: Create baner and about component
    Import font in style.css,
    Update assets.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BanerComponent } from './baner/baner.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BanerComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
