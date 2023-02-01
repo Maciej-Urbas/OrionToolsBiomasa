646e06fe5dff4ae50c44b4a27395e5f95f026ff6
Maciej Urbas <maciej.urbas3@gmail.com>
2023-02-01 21:00:23 +0100

    :sparkles: Add footer component with respons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { SwiperModule } from 'swiper/angular';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,10 +13,27 @@ import { SteelComponent } from './steel/steel.component';
 import { RecycleComponent } from './recycle/recycle.component';
 import { RegenComponent } from './regen/regen.component';
 import { NewestComponent } from './newest/newest.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, BanerComponent, AboutComponent, WoodComponent, SteelComponent, RecycleComponent, RegenComponent, NewestComponent],
-  imports: [BrowserModule, CommonModule, SwiperModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    BanerComponent,
+    AboutComponent,
+    WoodComponent,
+    SteelComponent,
+    RecycleComponent,
+    RegenComponent,
+    NewestComponent,
+    FooterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    SwiperModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
